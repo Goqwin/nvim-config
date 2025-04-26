625972a5c4a50aea9de99217b9737d40dc5ac4e6
Godwin M <net_win@NetWin.>
2025-04-25 18:10:49 -0600

    [FIX] My Z-shell crashed Ubuntu

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,49 +1,51 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  config = function()
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "lua_ls",
-        "rust_analyzer",
-        "pyright",
-        "ts_ls",
-        "clangd",
-        "gopls",
-	"html",
-	"marksman",
-      },
-    })
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "rust_analyzer",
+          "pyright",
+          "ts_ls",
+          "clangd",
+          "gopls",
+          "html",
+          "marksman",
+        },
+      })
 
-    local lspconfig = require("lspconfig")
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    local servers = {
-      lua_ls = {
-        settings = {
-          Lua = {
-            diagnostics = { globals = { "vim" } },
-            workspace = { checkThirdParty = false },
+      local servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = { globals = { "vim" } },
+              workspace = { checkThirdParty = false },
+            },
           },
         },
-      },
-      rust_analyzer = {},
-      pyright = {},
-      ts_ls = {},
-      clangd = {},
-      gopls = {},
-      html = {},
-	marksman = {},	
-    }
+        rust_analyzer = {},
+        pyright = {},
+        ts_ls = {}, 
+        clangd = {},
+        gopls = {},
+        html = {},
+        marksman = {},
+      }
 
-    for name, config in pairs(servers) do
-      config.capabilities = capabilities
-      lspconfig[name].setup(config)
-    end
-  end,
+      for name, config in pairs(servers) do
+        config.capabilities = capabilities
+        lspconfig[name].setup(config)
+      end
+    end,
+  },
 }
