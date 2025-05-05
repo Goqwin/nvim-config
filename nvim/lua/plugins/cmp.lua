bad492cb05e769b5cf8088d00bc847bbffdaaee4
Goqwin <godwinnmercado@hotmail.com>
2025-05-05 17:14:30 -0600

    [FEATURE] Adding formatter globally, to remove white space when saving.

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -4,9 +4,9 @@ return {
     "hrsh7th/cmp-nvim-lsp",
     "L3MON4D3/LuaSnip",
     "saadparwaiz1/cmp_luasnip",
-    "hrsh7th/cmp-path", 
-    "hrsh7th/cmp-buffer", 
-    "onsails/lspkind-nvim", 
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-buffer",
+    "onsails/lspkind-nvim",
     "rafamadriz/friendly-snippets",
   },
   config = function()
